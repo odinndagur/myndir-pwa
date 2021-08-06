7aa47662a62134ad8f39bc0cd3f5421636012ba8
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2021-08-06 09:53:03 +0000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,32 @@
 <template>
-
   <div id="app" class="container">
-
-    <div class="header"><h2>Myndir</h2></div>
+    <div class="header"><h2>🎥</h2></div>
 
     <div class="item movieInfo">
-    <div class="poster"><img v-if="foto" :src="foto" /></div>
-    <div class="text">
-    <h5 class="movie-title">{{title}}</h5>
-    <h5 class="year">{{year}}</h5>
-    <h6 class="genre">{{genre}}</h6>
-    <p class="plot">{{plot}}</p>
+      <div class="poster"><img v-if="foto" :src="foto" /></div>
+      <div class="text">
+        <h5 class="movie-title">{{ title }}</h5>
+        <h5 class="year">{{ year }}</h5>
+        <h6 class="genre">{{ genre }}</h6>
+        <p class="plot">{{ plot }}</p>
+      </div>
     </div>
-  </div>
 
+    <div class="item input">
+      <form @submit.prevent="searchMovie()">
+        <label for="movieName" class="mr-2">Movie name</label><br />
+        <input
+          v-model="movieQuery"
+          type="text"
+          class="form-control col-md-30"
+          id="movieInput"
+          placeholder="Three Billboards Outside Ebbing, Missouri"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
 
-
-  <!-- <div class="d-flex justify-content-md-center mt-5">
+    <!-- <div class="d-flex justify-content-md-center mt-5">
     <div class="float-md-left"><img v-if="foto" :src="foto" width="1rem" alt /></div>
 
     <div class="">
@@ -34,9 +44,9 @@
       </div>
     </div>
   </div> -->
-  <!-- <p v-if="movie" class="d-flex justify-content-md-center mt-3">You are adding&nbsp; <a v-bind:href="imdbLink"><b>{{ title }}</b></a>&nbsp; by director&nbsp;  <b>{{ director }}</b>, released in&nbsp;<b>{{ year }}</b>.</p> -->
+    <!-- <p v-if="movie" class="d-flex justify-content-md-center mt-3">You are adding&nbsp; <a v-bind:href="imdbLink"><b>{{ title }}</b></a>&nbsp; by director&nbsp;  <b>{{ director }}</b>, released in&nbsp;<b>{{ year }}</b>.</p> -->
 
-<div class="row">
+    <!-- <div class="row">
   <div class="mt-3 col-12">
     <div class="d-flex justify-content-md-center">
       <form class="form-inline" @submit.prevent=searchMovie()>
@@ -48,27 +58,18 @@
       </form>
     </div>
   </div>
-</div>
+</div> -->
 
-<div class="row">
-  <div class="mt-3 col-12">
-    <div class="d-flex justify-content-md-center">
-      <ul style="list-style:none">
-        <li v-for="(item, index) in moviesList" :key="item">
+    <ul style="list-style: none">
+      <li v-for="(item, index) in moviesList" :key="item">
+        <span>{{index+1}}</span><a v-on:click="selectMovie(index)">{{item.Title}}</a>
+        <!-- <form class="form-inline" @submit.prevent="selectMovie(index)">
+          <button type="submit" class="">👉</button>
+          <label for="movieName" class="mr-2">{{ item.Title }}</label>
+        </form> -->
+      </li>
+    </ul>
 
-            <form class="form-inline" @submit.prevent=selectMovie(index)>
-            <button type="submit" class="btn btn-primary btn-md mb-2">👉</button>
-        <div class="form-group mx-sm-3 mb-2">
-          <label for="movieName" class="mr-2">{{item.Title}}</label>
-        </div>
-
-      </form>
-
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>    
     <!-- <div class="item search-box">
       <form @submit.prevent="searchMovie()">
         <div class="form-group">
@@ -86,8 +87,6 @@
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
     </div> -->
-
-
   </div>
 </template>
 
@@ -96,60 +95,60 @@ export default {
   name: "Home",
   data() {
     return {
-      movieQuery: 'Three Billboards Outside Ebbing, Missouri',
+      movieQuery: "Three Billboards Outside Ebbing, Missouri",
       movie: {},
-      foto: '',
-      title: '',
-      plot: '',
-      genre: '',
-      imdbLink: '',
-      director: '',
-      year: '',
+      foto: "",
+      title: "",
+      plot: "",
+      genre: "",
+      imdbLink: "",
+      director: "",
+      year: "",
       moviesList: [],
     };
   },
   methods: {
     searchMovie() {
       fetch("https://www.omdbapi.com/?apikey=2d1ccbe&s=" + this.movieQuery)
-        .then(response => {
+        .then((response) => {
           return response.json();
         })
-        .then(data => {
+        .then((data) => {
           this.moviesList = data.Search;
           data = this.moviesList[0];
           fetch("https://www.omdbapi.com/?apikey=2d1ccbe&i=" + data.imdbID)
-            .then(response => {
+            .then((response) => {
               return response.json();
             })
-            .then(data => {
+            .then((data) => {
               console.log(data);
               this.foto = data.Poster;
               this.title = data.Title;
               this.plot = data.Plot;
               this.genre = data.Genre;
-              this.imdbLink = 'https://www.imdb.com/title/' + data.imdbID;
+              this.imdbLink = "https://www.imdb.com/title/" + data.imdbID;
               this.director = data.Director;
               this.year = data.Year;
             });
         });
       this.movieQuery = "";
     },
-    selectMovie(index){
+    selectMovie(index) {
       this.movie = this.moviesList[index];
       fetch("https://www.omdbapi.com/?apikey=2d1ccbe&i=" + this.movie.imdbID)
-            .then(response => {
-              return response.json();
-            })
-            .then(data => {
-              console.log(data);
-              this.foto = data.Poster;
-              this.title = data.Title;
-              this.plot = data.Plot;
-              this.genre = data.Genre;
-              this.imdbLink = 'https://www.imdb.com/title/' + data.imdbID;
-              this.director = data.Director;
-              this.year = data.Year;
-            });
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          this.foto = data.Poster;
+          this.title = data.Title;
+          this.plot = data.Plot;
+          this.genre = data.Genre;
+          this.imdbLink = "https://www.imdb.com/title/" + data.imdbID;
+          this.director = data.Director;
+          this.year = data.Year;
+        });
     },
   },
   created: function () {
@@ -159,16 +158,15 @@ export default {
 </script>
 
 <style scoped>
-.container{
+.container {
   display: grid;
   grid-template-columns: 100%;
   grid-template-rows: 80px auto 50px;
- } 
+}
 
-
- .item {
-   /* border:1px solid black; */
- }
+.item {
+  /* border:1px solid black; */
+}
 /* img {
   display: block;
   margin: auto;
@@ -180,66 +178,96 @@ export default {
   grid-column: 1;
   display: grid;
   grid-template-columns: 3fr auto;
-  grid-template-rows:25% 25% 25% 25%;
+  grid-template-rows: 25% 25% 25% 25%;
   /* border:1px solid white; */
   /* margin: auto;
   max-width:90%;
   min-height: 300px; */
   /* border: 1px solid black; */
   /* background: lightcoral; */
-    /* border: 5px solid red; */
-
+  /* border: 5px solid red; */
 }
-.poster{
+
+form {
+  margin-top: 1rem;
+}
+
+input {
+  width: 70%;
+  height: 2rem;
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+ul {
+  padding-top: 1rem;
+  font-size: 1.5rem;
+  text-align:left;
+}
+
+ul>li:nth-child(even){
+    color:grey
+}
+
+ul button {
+  margin: 0 10px;
+}
+
+ul a{
+  margin: 0 10px;
+}
+
+ul a:hover{
+  /* display:none; */
+}
+.poster {
   grid-template-columns: 1;
   grid-row: 1/4;
   /* float:left;
 border: 5px solid red; */
-/* width:50px; */
+  /* width:50px; */
 }
 
 .poster img {
   max-width: 200px;
 }
 
-.movieInfo .text{
+.movieInfo .text {
   /* grid-column:2; */
 }
 .movieInfo .movie-title {
-  grid-row:1;
+  grid-row: 1;
   grid-column: 2;
   align-self: top;
-  margin-top:5px;
-  margin-bottom:5px;
+  margin-top: 5px;
+  margin-bottom: 5px;
   /* border: 1px solid white; */
 }
 
 .movieInfo .genre {
-  margin-top:0;
-  margin-bottom:10px;
+  margin-top: 0;
+  margin-bottom: 10px;
   font-style: italic;
-  color:darkgray
+  color: darkgray;
 }
 
 .movieInfo .plot {
   font-size: 0.9rem;
   margin: 0 15px;
-text-align: justify;
-text-justify: inter-word;
+  text-align: left;
+  /* text-justify: inter-word; */
 }
 
 .movieInfo .year {
-  margin:0 0 10px 0;
+  margin: 0 0 10px 0;
 }
 
 .search-box {
-grid-row: 4;
-
+  grid-row: 4;
 }
 
 .header {
-grid-row:1;
-align-self: top;
+  grid-row: 1;
+  align-self: top;
 }
-
 </style>
